2f93874f52a1764e29ba559e84c9f3d042051dbc
basicmojosoftware <basicmojosoftware@gmail.com>
2025-05-07 14:56:55 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+'use client'
 import { useState } from 'react';
 import { Button } from "/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "/components/ui/card";
