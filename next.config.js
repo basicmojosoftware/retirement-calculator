33ddb47d91b6b6143736bbde51705ca0d7527f99
basicmojosoftware <basicmojosoftware@gmail.com>
2025-05-09 11:06:14 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Required for ShadCN UI path aliases
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    }
+    return config
+  },
+  // Fixes "Module not found" for ShadCN components
+  experimental: {
+    serverComponentsExternalPackages: [
+      '@radix-ui/react-dialog',
+      '@radix-ui/react-slot',
+      'class-variance-authority',
+      'clsx',
+      'tailwind-merge',
+    ],
+  },
 }
 
 module.exports = nextConfig
